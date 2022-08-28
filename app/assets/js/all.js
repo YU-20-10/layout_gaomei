e86b0a42ce8a41b30a62545a7632d99a9a4a6d95
YuJhen <j759153018@gmail.com>
2022-08-28 20:10:30 +0800

    index renew

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,10 @@
-$(function() {
+$(function () {
   console.log('Hello Bootstrap5');
+});
+
+// swipper
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });

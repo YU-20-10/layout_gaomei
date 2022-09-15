341bbed3488b92dd36df3b529f43467f1afb2d23
YuJhen <j759153018@gmail.com>
2022-09-16 01:54:20 +0800

    update all pages

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -8,3 +8,7 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
   },
 });
+
+//tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));

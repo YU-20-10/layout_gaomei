edf9583c46c10e099402a5727f0d3866933c8bd8
YuJhen <j759153018@gmail.com>
2022-09-16 23:55:50 +0800

    add font family & index swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -4,9 +4,16 @@ $(function () {
 
 // swipper
 var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   pagination: {
     el: ".swiper-pagination",
   },
+  mousewheel: true,
+  keyboard: true,
 });
 
 //tooltips

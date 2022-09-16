f107ad84b267b9149f13688c8f15e7f12a26dc5e
YuJhen <j759153018@gmail.com>
2022-09-16 23:57:14 +0800

    Update 2022-09-16T15:57:10.589Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -17,9 +17,16 @@ $(function () {
 }); // swipper
 
 var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
   pagination: {
     el: ".swiper-pagination"
-  }
+  },
+  mousewheel: true,
+  keyboard: true
 }); //tooltips
 
 var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
